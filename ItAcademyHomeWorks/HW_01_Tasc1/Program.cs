87a152ee12f53bc7fb7d3b854385674f9f701eef
YouRockPet <yourockpet@gmail.com>
2019-09-19 20:40:19 +0300

    HW_01_Task1:created new project

---
+++ ItAcademyHomeWorks/HW_01_Tasc1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW_01_Tasc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
