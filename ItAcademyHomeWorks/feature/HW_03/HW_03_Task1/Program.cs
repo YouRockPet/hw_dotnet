44785bb666aea8f509bdf695937c7e9d0c66652d
YuryPetruchenia <yourockpet@gmail.com>
2019-09-29 16:33:01 +0300

    Add my HW_03

---
+++ ItAcademyHomeWorks/feature/HW_03/HW_03_Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HW_03_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите чисто 1");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите чисто 2");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Calc(num1,num2)) ;
+
+            int Calc(int x, int y)
+            {
+                return x + y;
+            }
+        }
+    }
+}
