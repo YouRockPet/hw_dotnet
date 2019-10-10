ce9aeee7c8fed73f32cb2faa7778fa399f683810
YuryPetruchenia <yourockpet@gmail.com>
2019-10-10 23:16:00 +0300

    Add my HW_06

---
+++ ItAcademyHomeWorks/feature/HW_06_Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HW_06_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string text = Console.ReadLine();
+            text.Replace("O", "A");
+           string[] ar = text.Split(";");
+            foreach (var item in ar)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Is this the real life? Is this just fantasy; Caught in a landslide, no escape from reality; Open your eyes, look up to the skies and see; I'm just a poor boy, I need no sympathy; Because I'm easy come, easy go, little high, little low;
+        }
+    }
+}
---
+++ ItAcademyHomeWorks/feature/HW_06_Task2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HW_06_Task2
+{
+    class Program
+    {
+        public static void swap(string a, string b)
+        {
+            string temp = null;
+            temp = a;
+            b = a;
+            b = temp;
+        }
+
+        public static void deleter (List<string> list, int a)
+        {
+            list.RemoveAt(a);
+        }
+        static void Main(string[] args)
+        {
+            //Is the just fantasy
+            string str = Console.ReadLine();
+            string[] text = str.Split(" ");
+            List<string> words = text.ToList();
+            int maxlen = 0, minlen = 0, indexmax = 0, indexmin = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i].Length > maxlen)
+                {
+                    maxlen = text[i].Length;
+                    indexmax = i;
+                }
+            }
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i].Length < minlen)
+                {
+                    minlen = text[i].Length;
+                    indexmin = i;
+                }
+            }            
+                       
+            swap(words[indexmin], words[indexmax]);
+            deleter(words, indexmax);
+
+            //words.RemoveAt(indexmax);
+
+            foreach (var item in words)
+            {
+                Console.Write(item+" ");
+            }
+            //static void swap()
+            //{
+            //    string temp = null;
+            //    temp = words[indexmin];
+            //    words[indexmin] = words[indexmax];
+            //    words[indexmax] = temp;
+            //}
+        }
+    }
+}
