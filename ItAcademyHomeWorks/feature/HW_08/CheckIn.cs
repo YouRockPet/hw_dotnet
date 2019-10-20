d3045508f72b4ed6c963ca4993abd15f8a50b580
YuryPetruchenia <yourockpet@gmail.com>
2019-10-20 20:11:42 +0300

    Add my HW_08.

---
+++ ItAcademyHomeWorks/feature/HW_08/CheckIn.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class CheckIn
+{
+    private string locationName;
+
+    public CheckIn(string name) => Name = name;
+
+    public string Name
+    {
+        get => locationName;
+        set => locationName = value;
+    }
+
+}
